b5c23ea41450a77b736577a87491b14fe5d76032
Sandun Induranga <sandutharu40@gmail.com>
2023-01-19 09:58:01 +0530

    Bean Life Cycle

---
+++ _02_Bean_Life_Cycle/src/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,27 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.PojoOne;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public class AppInitializer {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+        PojoOne pojoOne = (PojoOne) ctx.getBean("pojoOne");
+        System.out.println(pojoOne);
+
+
+        ctx.close();
+
+    }
+
+}
